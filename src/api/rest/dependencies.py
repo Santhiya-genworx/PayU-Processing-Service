0041200e7256ffc58efc6ecdedf5dfd22458d89e
Santhiya-genworx <santhiyas@genworx.ai>
2026-04-07 18:15:15 +0530

    upgraded code

--- src/api/rest/dependencies.py
+++ src/api/rest/dependencies.py
@@ -1,13 +1,11 @@
 """Dependency module for the PayU Processing Service API.
 This module defines dependencies that can be injected into API route handlers. Currently, it includes a dependency for providing an asynchronous database session to route handlers that require database access. The get_db function is an asynchronous generator that yields a database session and ensures it is properly closed after use.
 """
-
 from collections.abc import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.data.clients.database import AsyncSessionLocal
-
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
     """Dependency that provides an asynchronous database session.
